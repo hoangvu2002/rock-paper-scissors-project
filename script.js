bdffd6bca6a1c185a379c0ce9a5552f4f4e6fcee
pdhoangvu <pdhoangvu@gmail.com>
2023-05-13 11:14:48 +0700

    Create div to display the result

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
+let playerCounter=0;
+let computerCounter=0;
+
+
+
+const rockButton = document.querySelector(".rock");
+rockButton.addEventListener("click", () => playRound("Rock", getComputerChoice()));
+
+const paperButton = document.querySelector(".paper");
+paperButton.addEventListener("click", () => playRound("Paper", getComputerChoice()));
+
+const scissorsButton = document.querySelector(".scissors");
+scissorsButton.addEventListener("click", () => playRound("Scissors", getComputerChoice()));
+
+div = document.querySelector("#result");
+div.textContent = "";
+
+const playerResult = document.createElement("p");
+playerResult.textContent = `${playerCounter}`;
+div.appendChild(playerResult);
+
+const computerResult = document.createElement("p");
+computerResult.textContent = `${computerCounter}`;
+div.appendChild(computerResult);
+
+if (computerCounter===5) {
+    div.textContent = "Computer is the winner";
+    computerCounter=0;
+    playerCounter=0;
+} else if (playerCounter===5) {
+    div.textContent="You are the winner";
+    computerCounter=0;
+    playerCounter=0;
+};
+
+
 function getComputerChoice() {
     computerChoice = ["Rock","Paper","Scissors"];
     choice=computerChoice[Math.floor(Math.random()*3)];
     return choice;
 }
-let playerCounter=0;
-let computerCounter=0;
+
 
 //let playerSelection = prompt("Enter your choice: ");
 //let computerSelection = getComputerChoice();
@@ -12,32 +47,41 @@ let computerCounter=0;
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.slice(0,1).toUpperCase()+playerSelection.slice(1).toLowerCase()==="Rock") {
         if (computerSelection ==="Rock") {
-            alert("Draw");
+            div.textContent=`Draw 
+            Player score:${playerCounter} Computer score: ${computerCounter}`;
         } else if (computerSelection ==="Paper") {
-            alert("You lost");
+            div.textContent=`You lost! 
+            Player score:${playerCounter} Computer score: ${computerCounter}`;
             computerCounter = ++computerCounter;
         } else {
-            alert("You won");
+            div.textContent=`You won! 
+            Player score:${playerCounter} Computer score: ${computerCounter}`;
             playerCounter=++playerCounter;
         };
     } else if (playerSelection.slice(0,1).toUpperCase()+playerSelection.slice(1).toLowerCase()==="Paper") {
         if (computerSelection ==="Paper") {
-            alert("Draw");
+            div.textContent=`Draw 
+            Player score:${playerCounter} Computer score: ${computerCounter}`;
         } else if (computerSelection ==="Scissors") {
-            alert("You lost");
+            div.textContent=`You lost! 
+            Player score:${playerCounter} Computer score: ${computerCounter}`;
             computerCounter = ++computerCounter;
         } else {
-            alert("You won");
+            div.textContent=`You won! 
+            Player score:${playerCounter} Computer score: ${computerCounter}`;
             playerCounter=++playerCounter;
         };
     } else if (playerSelection.slice(0,1).toUpperCase()+playerSelection.slice(1).toLowerCase()==="Scissors") {
         if (computerSelection ==="Scissors") {
-            alert("Draw");
+            div.textContent=`Draw 
+            Player score:${playerCounter} Computer score: ${computerCounter}`;
         } else if (computerSelection ==="Rock") {
-            alert("You lost");
+            div.textContent=`You lost! 
+            Player score:${playerCounter} Computer score: ${computerCounter}`;
             computerCounter = ++computerCounter;
         } else {
-            alert("You won");
+            div.textContent=`You won! 
+            Player score:${playerCounter} Computer score: ${computerCounter}`;
             playerCounter=++playerCounter;
         };
     } else {
@@ -72,13 +116,4 @@ function game() {
     };
     //I change the condition from 3 to 1 so that this game only play one round
 };
-
-const rockButton = document.querySelector(".rock");
-rockButton.addEventListener("click", () => playRound("Rock", getComputerChoice()));
-
-const paperButton = document.querySelector(".paper");
-paperButton.addEventListener("click", () => playRound("Paper", getComputerChoice()));
-
-const scissorsButton = document.querySelector(".scissors");
-scissorsButton.addEventListener("click", () => playRound("Scissors", getComputerChoice()));
 
